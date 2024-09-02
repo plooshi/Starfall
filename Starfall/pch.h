01e2d951891562614482ddc2c55489c83c3236b5
ploosh <me@ploosh.dev>
2024-09-02 14:05:43 -0400

    (2/3) fix build

--- Starfall/pch.h
+++ Starfall/pch.h
@@ -10,6 +10,37 @@
 #include <string_view>
 #include <string>
 
+template <size_t _Sz>
+struct String {
+    char _St[_Sz];
+
+    consteval String(const char(&_Ps)[_Sz])
+    {
+        std::copy_n(_Ps, _Sz, _St);
+    }
+
+    operator const char* () {
+        return _St;
+    }
+
+    constexpr std::string_view StringView() const {
+        return _St;
+    }
+};
+
+template <typename _Ft>
+struct Func {
+    _Ft _Fn;
+
+    consteval Func(_Ft _Pf)
+    {
+        _Fn = _Pf;
+    }
+
+    constexpr _Ft Get() const {
+        return _Fn;
+    }
+};
 namespace Plooshfinder {
 	#include "../plooshfinder/include/plooshfinder_sig.h"
 	#include "../plooshfinder/include/plooshfinder.h"
