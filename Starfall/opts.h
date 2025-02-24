1366a00f4afa22d7880a19d095e606dcf206fd46
Sarah <me@ploosh.dev>
2025-02-24 12:18:41 -0500

    clarify

--- Starfall/opts.h
+++ Starfall/opts.h
@@ -4,7 +4,7 @@
 constexpr bool Console = false; // if the console window should be created
 constexpr StarfallURLSet URLSet = Default;
 constexpr inline FString Backend = L"http://127.0.0.1:3551"; // your backend url
-constexpr bool bHasPushWidget = false; // enable if you have gs closing after a couple seconds of listening or cheat/RE detected w/o any open. breaks closing the client.
+constexpr bool bHasPushWidget = false; // enable if you have gs closing after a couple seconds of listening. breaks closing the client (so don't build with this enabled for usage in a launcher.)
 
 // misc options, don't change unless you know what you're doing
 constexpr bool UseBackendParam = false; // for phoenix/paradise launcher
